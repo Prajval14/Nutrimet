1ad9586c78280c92997e9bb2959a84a91a1cb3ac
Deep2410 <deep24102410@gmail.com>
2024-03-24 14:33:17 -0400

    cart.css and cartjs updated

--- js/cart.js
+++ js/cart.js
@@ -17,8 +17,8 @@ var cartProductContainer = document.getElementById('p');
 function createCards(data){
     const cardDiv = document.createElement('div');
     cardDiv.innerHTML = `
-        <div class="card mb-3" >
-        <div class="row no-gutters">
+    <div class="card mb-3" >
+    <div class="row no-gutters">
         <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6">
             <img class="card-img img-thumbnail" src="${data.imageURL}" alt="product Image" width="100%">
         </div>
@@ -27,18 +27,20 @@ function createCards(data){
                 <h5 class="card-title">${data.productname}</h5>
                 <p class="card-text mb-1"><small class="text-muted">${data.productdetail}</small></p>
                 <p class="price-cart fs-5 fw-bold p-0 m-0" id="discounted-price">$ ${data.originalprice}</p>
-                <div class="d-flex justify-content-end align-items-end mt-auto"  id="edit-product-cart">
-                
-                    <!-- <div> -->
+                <div class="d-flex justify-content-between align-items-end mt-auto"  id="edit-product-cart">
+                    <span>
+                    <label for="qty">Qty:</label>
+                    <input type="number" id="cart_qty" min="1" max="20" value="1">
+                    </span>
+                    <span>
                         <i class="bi bi-trash3 fs-4 bg" data-toggle="tooltip" data-placement="bottom" title="Delete from the cart"></i>
                         <i class="bi bi-bookmark-heart fs-4" data-toggle="tooltip" data-placement="bottom" title="Add to wish list"></i>
-                    <!-- </div> -->
-                    
+                    </span>                                    
                 </div>
             </div>
         </div>
-        </div>
     </div>
+</div>
     `;
     cartProductContainer.appendChild(cardDiv);
 }

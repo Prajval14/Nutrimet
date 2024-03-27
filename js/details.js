d74a46d72f718be2263d4f0d6577c2c61b410922
Urvesh Patel <patelurvesh998@gmail.com>
2024-03-27 15:21:27 -0400

    validation update

---
+++ js/details.js
@@ -0,0 +1,27 @@
+function onPageLoad()
+{
+  var F_Name =  sessionStorage.getItem('f_name');
+  var L_Name = sessionStorage.getItem('l_name');
+  var Email = sessionStorage.getItem('userName_R');
+  var Password = sessionStorage.getItem('password_R');
+
+  var Full_Name = F_Name +" "+ L_Name ;
+
+
+  var name = document.getElementById('Full_Name');
+  var email = document.getElementById('email');
+  var password = document.getElementById('password')
+  name.value = Full_Name;
+  email.value = Email;
+  password.value = Password;
+
+
+
+
+
+}
+  document.addEventListener("DOMContentLoaded", function(event) {
+    // Call onPageLoad when the DOM content is fully loaded
+    onPageLoad();
+    
+});

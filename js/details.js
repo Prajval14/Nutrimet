3d56b0c548dccae6eaf9e9b2b573745fefc139a1
Prajval Patel <prajval1420@gmail.com>
2024-04-04 09:54:28 -0400

    details update #fbab0f1

--- js/details.js
+++ js/details.js
@@ -3,13 +3,27 @@ function onPageLoad() {
   var L_Name = sessionStorage.getItem("l_name");
   var Email = sessionStorage.getItem("userName_R");
   var Password = sessionStorage.getItem("password_R");
+
   var Full_Name = F_Name + " " + L_Name;
+
   var name = document.getElementById("Full_Name");
   var email = document.getElementById("email");
   var password = document.getElementById("password");
   name.value = Full_Name;
   email.value = Email;
   password.value = Password;
+}
+
+function updateDetails() {
+  var delAddress = document.getElementById("address").value;
+
+  sessionStorage.setItem("userAddress", delAddress);
+  // Call displayMessage function with appropriate title and content
+  var myModal = new bootstrap.Modal(
+    document.getElementById("successModal"),
+    {}
+  );
+  myModal.show();
 }
 
 function closeDetails() {
@@ -21,7 +35,24 @@ function resetSession() {
   window.location.href = "../index.html";
 }
 
+function displayMessage(title, content) {
+  var modalTitle = document.getElementById("commonMsgModalLabel");
+  var modalContent = document.getElementById("commonMsgContent");
+
+  modalTitle.innerText = title;
+  modalContent.innerText = content;
+
+  $("#commonMsgModal").modal("show");
+}
+
+// Example usage:
+// displayMessage("Success!", "Registration successful! Your registration was successful. Thank you!");
+
 document.addEventListener("DOMContentLoaded", function (event) {
   // Call onPageLoad when the DOM content is fully loaded
   onPageLoad();
+});
+
+document.querySelector("#updateButton").addEventListener("click", function () {
+  updateDetails();
 });

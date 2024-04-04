2471608d87ab4d6f01b83ff0a9b152fa963ee346
Prajval Patel <prajval1420@gmail.com>
2024-04-04 09:48:40 -0400

    updates css #c0783c0

--- js/details.js
+++ js/details.js
@@ -1,27 +1,27 @@
-function onPageLoad()
-{
-  var F_Name =  sessionStorage.getItem('f_name');
-  var L_Name = sessionStorage.getItem('l_name');
-  var Email = sessionStorage.getItem('userName_R');
-  var Password = sessionStorage.getItem('password_R');
-
-  var Full_Name = F_Name +" "+ L_Name ;
-
-
-  var name = document.getElementById('Full_Name');
-  var email = document.getElementById('email');
-  var password = document.getElementById('password')
+function onPageLoad() {
+  var F_Name = sessionStorage.getItem("f_name");
+  var L_Name = sessionStorage.getItem("l_name");
+  var Email = sessionStorage.getItem("userName_R");
+  var Password = sessionStorage.getItem("password_R");
+  var Full_Name = F_Name + " " + L_Name;
+  var name = document.getElementById("Full_Name");
+  var email = document.getElementById("email");
+  var password = document.getElementById("password");
   name.value = Full_Name;
   email.value = Email;
   password.value = Password;
-
-
-
-
-
 }
-  document.addEventListener("DOMContentLoaded", function(event) {
-    // Call onPageLoad when the DOM content is fully loaded
-    onPageLoad();
-    
+
+function closeDetails() {
+  window.location.href = "../index.html";
+}
+
+function resetSession() {
+  sessionStorage.clear();
+  window.location.href = "../index.html";
+}
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  // Call onPageLoad when the DOM content is fully loaded
+  onPageLoad();
 });

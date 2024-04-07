144a3a8a8781602bf5f550aca8465f17c56a7486
Prajval Patel <prajval1420@gmail.com>
2024-04-07 18:06:19 -0400

    removed redundant code

--- js/products.js
+++ js/products.js
@@ -29,22 +29,22 @@ function handleAddToCart() {
 }
 
 function addProductToCart(event) {
-    //Set add to cart on click to addded for few seconds
-    const button = event.target;
-    button.innerHTML = `Added <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+  //Set add to cart on click to addded for few seconds
+  const button = event.target;
+  button.innerHTML = `Added <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
     <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
     </svg>`;
-    button.classList.add('added');
-    setTimeout(function() {
-        button.textContent = 'Add to Cart';
-        button.classList.remove('added');
-    }, 1000);
+  button.classList.add('added');
+  setTimeout(function () {
+    button.textContent = 'Add to Cart';
+    button.classList.remove('added');
+  }, 1000);
 
-    //Add product ID to cart and a list 
-    const productID = event.target.closest('.card').querySelector('.product-id').textContent;        
-    myCart.push(productID);
-    navbarBadge.style.display = 'flex';
-    cartBadge.innerHTML = myCart.length;
+  //Add product ID to cart and a list 
+  const productID = event.target.closest('.card').querySelector('.product-id').textContent;
+  myCart.push(productID);
+  navbarBadge.style.display = 'flex';
+  cartBadge.innerHTML = myCart.length;
 }
 
 function generateStarRating(rating) {
@@ -64,9 +64,8 @@ function displayProducts(products, productsContainer) {
     const productHTML = `
         <div class="col-md-3 mb-4">
                 <div class="card h-100">
-                    <img src="${product.imageURL}" class="card-img-top" alt="${
-      product.productname
-    }">
+                    <img src="${product.imageURL}" class="card-img-top" alt="${product.productname
+      }">
                     <div class="card-body">
                         <h5 class="product-id d-none">${product.productid}</h5>
                         <h5 class="card-title">${product.productname}</h5>
@@ -75,12 +74,10 @@ function displayProducts(products, productsContainer) {
                             ${generateStarRating(product.rating)}
                         </div>
                         <p class="card-price">
-                            <span class="text-decoration-line-through text-secondary fw-light">$${
-                              product.originalprice
-                            }</span>
-                            <span class="fw-bold text-danger ps-1 fs-5">$${
-                              product.discountPrice || product.originalprice
-                            }</span>
+                            <span class="text-decoration-line-through text-secondary fw-light">$${product.originalprice
+      }</span>
+                            <span class="fw-bold text-danger ps-1 fs-5">$${product.discountPrice || product.originalprice
+      }</span>
                         </p>
                         <button class="btn btn-primary border-0 fw-medium add_to_cart_button">Add to cart</button>
                         <a href="#" class="btn btn-link" style="font-size: 0.7rem;">View Product Details</a>
@@ -122,6 +119,6 @@ document
   .addEventListener(
     "click",
     () =>
-      (window.location.href =
-        "./cart.html?index_page_selected_products=" + JSON.stringify(myCart))
+    (window.location.href =
+      "./cart.html?index_page_selected_products=" + JSON.stringify(myCart))
   );

cda3f30ddcc873706319ff0fde35a7cf3d2f7f0b
Urvesh Patel <patelurvesh998@gmail.com>
2024-04-08 16:05:34 -0400

    style changes

--- js/signup.js
+++ js/signup.js
@@ -39,46 +39,95 @@ function changeText() {
 // Change text content every 3 seconds
 setInterval(changeText, 3000);
 
-function validateForms() {
+
+function errorValidate(isError)
+{ 
+
+  var f_Name = document.getElementById("f_name").value.trim();
+  var l_Name = document.getElementById("l_name").value.trim();
   var password = document.getElementById("password").value;
   var confirmPassword = document.getElementById("cnfpassword").value;
+  var email = document.getElementById("email").value.trim();
+  var containsSpecialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(password);
+  var emailVerify =  /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
+  var errors = false;
+  
+    
+    if (f_Name === '') {
+        document.getElementById('firstNameError').innerText = 'Please enter your first name';
+        errors = true;
+    } else {
+        document.getElementById('firstNameError').innerText = '';
+    }
+    if (l_Name === '') {
+        document.getElementById('lastNameError').innerText = 'Please enter your last name';
+        errors = true;
+    } else {
+        document.getElementById('lastNameError').innerText = '';
+    }
+    if (password === '') {
+        document.getElementById('passwordError').innerText = 'Please enter a password';
+        errors = true;
+    }
+    else if(!containsSpecialChar){
+        document.getElementById('passwordError').innerText=("Password must contain at least one number and one special character.");
+        return false;  
+    } 
+    else {
+        document.getElementById('passwordError').innerText = '';
+    }
+
+    if (confirmPassword === '') {
+        document.getElementById('confirmPasswordError').innerText = 'Please confirm your password';
+        errors = true;
+    } 
+    else if (password !== confirmPassword) {
+        document.getElementById('confirmPasswordError').innerText = 'Passwords do not match';
+        errors = true;
+    } else {
+        document.getElementById('confirmPasswordError').innerText = '';
+    }
+
+    if (email === '') {
+      document.getElementById('emailError').innerText = 'Please enter your email';
+      errors = true;
+  }
+  else if(!emailVerify){
+      document.getElementById('emailError').innerText=("Not a Valid Email.");
+      return false;  
+  }  
+  else {
+      document.getElementById('emailError').innerText = '';
+  }
+
+    if(!errors)
+    {
+      isError = true;
+    }
+return isError;
+}
+
+function validateForms() {
   var f_Name = document.getElementById("f_name").value;
   var l_Name = document.getElementById("l_name").value;
 
-  // Check if passwords match
-  if (password !== confirmPassword) {
-    alert("Passwords do not match.");
-    return false;
-  }
-
-  // Check password strength (contains at least one number and one special character)
-  var containsNumber = /\d/.test(password);
-  var containsSpecialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(
-    password
-  );
-
-  if (!containsNumber || !containsSpecialChar) {
-    alert(
-      "Password must contain at least one number and one special character."
+  var isError = false;
+  isError = errorValidate(isError);
+  if(isError)
+  {
+    var userId = document.getElementById("email").value;
+    var passWordId = document.getElementById("password").value;
+    sessionStorage.setItem("userName_R", userId);
+    sessionStorage.setItem("password_R", passWordId);
+    sessionStorage.setItem("f_name", f_Name);
+    sessionStorage.setItem("l_name", l_Name);
+    var myModal = new bootstrap.Modal(
+      document.getElementById("successModal"),
+      {}
     );
-    return false;
+    myModal.show();
   }
-
-  function closeSuccessCard() {
-    document.getElementById("successCard").style.display = "none";
-  }
-
-  var userId = document.getElementById("email").value;
-  var passWordId = document.getElementById("password").value;
-  sessionStorage.setItem("userName_R", userId);
-  sessionStorage.setItem("password_R", passWordId);
-  sessionStorage.setItem("f_name", f_Name);
-  sessionStorage.setItem("l_name", l_Name);
-  var myModal = new bootstrap.Modal(
-    document.getElementById("successModal"),
-    {}
-  );
-  myModal.show();
+ 
 
   return false;
 }
@@ -98,8 +147,34 @@ function loginValidation() {
     window.location.href = "../index.html";
     sessionStorage.setItem("isValid", true);
   } else {
-    alert("Invalid email or password. Please try again.");
+    if (userName === '') {
+      document.getElementById('loginError').innerText = 'Please enter your Email';
+      errors = true;
+  } 
+    else if(userName != R_Id)
+    {
+      document.getElementById('loginError').innerText = 'UserName Not Found!!!';
+      errors = true;
+    }
+    else {
+      document.getElementById('loginError').innerText = '';
+    }
+  
+    if(l_Password === '')
+    {
+      document.getElementById('passError').innerText = 'Please enter your Password';
+      errors = true;
+    }
+    else if (l_Password != R_Pass )
+    {
+      document.getElementById('passError').innerText = 'Wrong Password!!!';
+      return false;
+    }
+    else {
+      document.getElementById('passError').innerText = '';
+    }
   }
+
 
   return false;
 }
